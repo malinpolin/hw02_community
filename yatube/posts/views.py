0bc285b33d333c4c8d36658ea975cb46e19bddb7
Полина Дорошенко <malinpolin@gmail.com>
2022-02-12 02:00:44 +0300

    2nd draft project sprint 3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -12,6 +12,7 @@ def index(request):
     }
     return render(request, template, context)
 
+
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     template = 'posts/group_list.html'
